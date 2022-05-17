3106e18c3541ae236282a998848e5dc5e6672a2e
GarunWindtotem <63738678+GarunWindtotem@users.noreply.github.com>
2022-05-17 21:13:08 +0200

    u

---
+++ Hello_World/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    // main
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int myAge = 34;
+        System.out.println("age= " + myAge);
+        int herAge = 28;
+        System.out.println("herAge= " + herAge);
+    }
+}
